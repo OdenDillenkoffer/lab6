27a12c0059163d763c4a62a398682e6957f8657b
kingofnerds1453 <149018767+kingofnerds1453@users.noreply.github.com>
2023-10-31 14:29:00 -0400

    i forgot to make encode() a function

--- version_control.py
+++ version_control.py
@@ -4,6 +4,36 @@ def menu():
     print("-" * 13)
     print("1. Encode\n2. Decode\n3. Quit\n")
 
+def encode():
+    usable_list = []
+    to_encode = input("Please enter your password to encode:")
+    for element in to_encode:
+        usable_list.append(element)
+    # turns the string into a list so i can actually mess with it
+    n = 0
+    for element in usable_list:
+        usable_list[n] = int(usable_list[n])
+        usable_list[n] = usable_list[n] + 3
+        # shifts each number up 3
+        n += 1
+    n = 0
+    for element in usable_list:
+        # deals with any integer above 9 and shifts them down to where they should be
+        if element >= 10:
+            if element == 10:
+                usable_list[n] = 0
+            elif element == 11:
+                usable_list[n] = 1
+            elif element == 12:
+                usable_list[n] = 2
+        n += 1
+    n = 0
+    for element in usable_list:
+        usable_list[n] = str(usable_list[n])
+        n += 1
+    encoded = "".join(usable_list)
+    return encoded
+    # creates the encoded password and saves it as a string
 
 # Decodes encoded password by adding 7 to the existing digit, bringing the total addition to 10. Modulo by 10.
 def decode(encoded_password):
@@ -22,34 +52,7 @@ while run_loop:
     choice = input("Please enter a option: ")
     choice = int(choice)
     if choice == 1:
-        usable_list = []
-        to_encode = input("Please enter your password to encode:")
-        for element in to_encode:
-            usable_list.append(element)
-        # turns the string into a list so i can actually mess with it
-        n = 0
-        for element in usable_list:
-            usable_list[n] = int(usable_list[n])
-            usable_list[n] = usable_list[n] + 3
-            # shifts each number up 3
-            n += 1
-        n = 0
-        for element in usable_list:
-            # deals with any integer above 9 and shifts them down to where they should be
-            if element >= 10:
-                if element == 10:
-                    usable_list[n] = 0
-                elif element == 11:
-                    usable_list[n] = 1
-                elif element == 12:
-                    usable_list[n] = 2
-            n += 1
-        n = 0
-        for element in usable_list:
-            usable_list[n] = str(usable_list[n])
-            n += 1
-        encoded = "".join(usable_list)
-        # creates the encoded password and saves it as a string
+        encoded = encode()
         print("Your password has been encoded and stored!\n")
     elif choice == 2:
         decode(encoded)
